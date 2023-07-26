084b287b886dd3d7cda1cd335dc4f7a7ea929746
shubham pathak <95shubham1@gmail.com>
2023-07-26 08:48:36 +0530

    init commit

---
+++ StringsBasics.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	string s;
+	 getline(cin,s,'.');
+	
+	cout<<s<<endl;
+	
+	
+	for(char ch : s){
+		cout<<ch <<",";
+	}
+	
+	
+	//vector<string>
+}
