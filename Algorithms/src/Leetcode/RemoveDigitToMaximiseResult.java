ff4a07a4a25764f615eaf136300a8314fe0bc291
shubham pathak <95shubham1@gmail.com>
2023-08-02 08:03:39 +0530

    Agust 200 questions challenge

---
+++ Algorithms/src/Leetcode/RemoveDigitToMaximiseResult.java
@@ -0,0 +1,23 @@
+package Leetcode;
+
+public class RemoveDigitToMaximiseResult {
+
+    public static void main(String[] args){
+        String number = "123";
+        String ans="";
+        char digit='3';
+        for(int i=0;i< number.length();i++){
+            if(number.charAt(i) == digit){
+                String temp = number.substring(0,i) + number.substring(i+1);
+                if(ans.equals("")){
+                    ans = temp;
+                }else{
+                    if(temp.compareTo(ans) > 0){
+                        ans = temp;
+                    }
+                }
+            }
+        }
+        System.out.printf(ans);
+    }
+}
