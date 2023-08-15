b8d49c7eb0c51706db8932f6fc23a406aadeb3c5
shubham pathak <95shubham1@gmail.com>
2023-08-15 11:18:56 +0530

    empty queue

--- circular queue.cpp
+++ circular queue.cpp
@@ -6,10 +6,12 @@ int front = -1, rear = -1, n=5;
 
 void insertCQ(int val) {
    if ((front == 0 && rear == n-1) || (front == rear+1)) {
+   	   // queue is full
       cout<<"Queue Overflow \n";
       return;
    }
    if (front == -1) {
+   	  // empty queue case
       front = 0;
       rear = 0;
    } else {
