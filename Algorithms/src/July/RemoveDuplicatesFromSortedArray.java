788a79358c260c78bc4567d35695eaf92c24f8f0
shubham pathak <95shubham1@gmail.com>
2023-08-15 08:38:07 +0530

    remove dup

--- Algorithms/src/July/RemoveDuplicatesFromSortedArray.java
+++ Algorithms/src/July/RemoveDuplicatesFromSortedArray.java
@@ -5,5 +5,19 @@ public class RemoveDuplicatesFromSortedArray {
 
     public static void main(String[] args){
 
+        int [] arr ={};
+
+        int j=0;
+
+        for(int i=0;i<n-1;i++){
+            if(arr[i]!=arr[i+1]){
+                arr[j++]=arr[i];
+            }
+        }
+        arr[j++]=arr[arr.length-1];
+        for(int i=0;i<j;i++){
+            System.out.println(arr[i]);
+        }
+       // System.out.println(j);
     }
 }
