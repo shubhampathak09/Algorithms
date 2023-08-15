54262f4c18695724bbfa5a9cd89b40204ac9d975
shubham pathak <95shubham1@gmail.com>
2023-08-15 10:00:49 +0530

    knm

--- knightMoves.cpp
+++ knightMoves.cpp
@@ -21,11 +21,38 @@ int dy[] ={1,2,2,1,-1,-2,-2,-1};
 
 
 int vis[10][10];
+
 int dis[10[10];
 
 int taretX,targetY;
 
 int main(){
+
+int x,y,q;
+
+cin>>q;
+
+while(q--){
+	
+	
+	char a,b;
+	
+	for(int i=0;i<=8;i++){
+		for(int j=0;j<=8;j++){
+			vis[i]][j]=0;
+		}
+	}
+	
+	cin>>a>>b;
+	
+	x = a -'0'; //getX(a);
+	y=  b- '0';
+	
+	
+	
+	
+	
+}
 
 
 	
