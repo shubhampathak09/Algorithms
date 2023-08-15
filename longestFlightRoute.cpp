c1f2da72cf6be86dddcb5aaf98713dc9a39e7dc2
shubham pathak <95shubham1@gmail.com>
2023-08-15 10:17:43 +0530

    longest path in node

--- longestFlightRoute.cpp
+++ longestFlightRoute.cpp
@@ -7,6 +7,8 @@ using namespace std;
 // application of dfs
 // dp
 // top down recursiove solution first
+// find out longest parth from each node to n
+// then over max will max of all of them and this will be our ans
 
 
 
