bae723863955a9742ac3c18d04cf382c616110a8
shubham pathak <95shubham1@gmail.com>
2023-08-15 10:34:37 +0530

    dp logic

--- longestFlightRoute.cpp
+++ longestFlightRoute.cpp
@@ -11,6 +11,24 @@ vector<int> adj[2000001];
 int next_node[20000001];
 
 
+
+void dfs(int node){
+	vis[node]=true;
+	
+	for(int v : adj[node]){
+		
+		if(vis[v]==false)
+		dfs(v);
+		
+		if(dp[v]!=-1 && dp[v] +1 > dp[node]){
+			dp[node]=dp[v]+1;
+			next+node[node]=v;
+		}
+		
+	}
+	
+}
+
 // application of dfs
 // dp
 // top down recursiove solution first
