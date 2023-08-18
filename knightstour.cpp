46bd37ba1f6888a82d0bca6e4cc8b45b58bee530
shubham pathak <95shubham1@gmail.com>
2023-08-18 10:27:06 +0530

    knights tour

---
+++ knightstour.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int issafe(int x,int y,int sol[N][N]){
+	
+	if(x>=0 && x< N && y>=0 && y< N && sol[x][y]!=-1){
+		return 1;
+	}else
+	return 0;
+	
+}
+
+
+
+boolean solveKtUtil(int x, int y,int moveNo,int sol[N][N]){
+	
+	int k,next_x,next_y;
+	
+	if(moveNo == N * N){
+		return 1;
+	}
+	
+	
+	for(k =0 ;k<8;k++){
+		next_x = x + dx[k];
+		next_y = y +dy[k];
+		if(issafe(next_x,next_y,sol)){
+			sol[next_x][next_y]=moveNo;
+			if(solveKtUtil(next_x,next_y,moveNo+1,sol)==1){
+				return 1
+			}else
+			{
+				sol[next_x][next_y]=-1;
+			}
+		}
+	}
+	
+	return 0;
+}
+
+
+int solveKt(){
+	int sol[N][N];
+	
+	for(int x =0 ;x<N;x++){
+		for(int y=0;y<N; y++){
+			sol[x][y]=-1;
+		}
+	}
+	
+	sol[0][0]=0;
+	
+	if(solveKtUtil(0,0,1,sol)==0){
+		cout<<"No solution"<<endl;
+		return 0;
+	}
+	else
+	printSolution(sol);
+	
+	return 1;
+	
+}
+
+int dx[]={2,1,-1,-2,-2,-1,1,2};
+int dy[]={1,2,2,1,-1,-2,-2,-1};
+
+
+
+
+int main(){
+	
+	
+	
+	solveKt();
+	return 0;
+}
