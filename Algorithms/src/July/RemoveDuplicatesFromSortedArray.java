9cf5c49e781e283997fc60c109f34e3aad6f7ffb
shubham pathak <95shubham1@gmail.com>
2023-08-20 09:21:21 +0530

    fixes

--- Algorithms/src/July/RemoveDuplicatesFromSortedArray.java
+++ Algorithms/src/July/RemoveDuplicatesFromSortedArray.java
@@ -9,7 +9,7 @@ public class RemoveDuplicatesFromSortedArray {
 
         int j=0;
 
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<arr.length-1;i++){
             if(arr[i]!=arr[i+1]){
                 arr[j++]=arr[i];
             }
